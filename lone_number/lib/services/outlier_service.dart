7091b716e6f691b0369cc3e72ca2c62e50d4a625
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2023-08-12 08:47:46 +0200

    Changes to be committed: 	modified:   lone_number/lib/services/outlier_service.dart
    Changes not staged for commit:
    	modified:   lone_number/android/build.gradle
    	modified:   lone_number/ios/Podfile.lock
    	modified:   lone_number/ios/Runner.xcodeproj/project.pbxproj
    	modified:   lone_number/pubspec.lock

--- lone_number/lib/services/outlier_service.dart
+++ lone_number/lib/services/outlier_service.dart
@@ -3,31 +3,28 @@ import '../config/models/number_list_model.dart';
 class OutlierService {
   int findOutlier(NumberList numberList) {
     final numbers = numberList.numbers;
-    final median = _calculateMedian(numbers);
-
-    num maxDeviation = 0;
-    var outlierValue = numbers[0]; // Initialize with the first number
+    int evenCount = 0;
+    int oddCount = 0;
+    int outlierValue = numbers[0]; // Initialize with the first number
 
     for (var number in numbers) {
-      final deviation = (number - median).abs();
-      if (deviation > maxDeviation) {
-        maxDeviation = deviation;
+      if (number % 2 == 0) {
+        evenCount++;
         outlierValue = number;
+      } else {
+        oddCount++;
+        outlierValue = number;
+      }
+
+      if (evenCount > 1 && oddCount == 1) {
+        break; // Found the outlier
+      }
+
+      if (oddCount > 1 && evenCount == 1) {
+        break; // Found the outlier
       }
     }
 
     return outlierValue;
-  }
-
-  num _calculateMedian(List<int> numbers) {
-    final sortedNumbers = [...numbers]..sort();
-    final n = sortedNumbers.length;
-
-    if (n % 2 == 1) {
-      return sortedNumbers[n ~/ 2].toInt();
-    } else {
-      final mid = n ~/ 2;
-      return (sortedNumbers[mid - 1] + sortedNumbers[mid]) / 2.0;
-    }
   }
 }
